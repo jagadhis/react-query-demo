e64fce6e81f572f4057ad874dcbba71d501d82b3
jagadhis <jwaran78@gmail.com>
2022-08-29 19:15:00 +0530

    Fixed The Eror

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -6,18 +6,21 @@ function Characters() {
         const response = await fetch("https://rickandmortyapi.com/api/character")
         const data = await response.json();
         console.log(data);
-        setCharacters(data)
+        setCharacters(data.results)
     }
 
     useEffect(() => {
         fetchCharacters();
     }, [])
     return (
-        <div>
-            {characters.results.map((character)=>(
-                <div>{character.name}</div>
-            ))}
-        </div>
+       <div>
+        {characters.map((character)=>{
+            return(
+            <div>{character.name}</div>
+            )
+        })}
+         
+       </div>
     )
 }
 
